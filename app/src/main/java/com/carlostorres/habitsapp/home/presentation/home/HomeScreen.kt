7d2d32d7f7290c74a19ccf7e2f8714cf612daea4
carlosttorres33 <kisamehatake33@comunidad.unam.mx>
2024-04-13 19:36:54 -0600

    Tercera y ultima parte de Presentation de la pantalla HomeScreen / HabitList

--- app/src/main/java/com/carlostorres/habitsapp/home/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/carlostorres/habitsapp/home/presentation/home/HomeScreen.kt
@@ -2,12 +2,15 @@ package com.carlostorres.habitsapp.home.presentation.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -26,6 +29,7 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.carlostorres.habitsapp.R
 import com.carlostorres.habitsapp.home.presentation.home.components.HomeDateSelector
+import com.carlostorres.habitsapp.home.presentation.home.components.HomeHabit
 import com.carlostorres.habitsapp.home.presentation.home.components.HomeQuote
 import java.time.ZonedDateTime
 
@@ -65,41 +69,58 @@ fun HomeScreen(
         }
     ){ paddingValues ->
 
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .padding(paddingValues)
-                .padding(20.dp),
-            verticalArrangement = Arrangement.spacedBy(19.dp)
+                .padding(start = 20.dp),
+            verticalArrangement = Arrangement.spacedBy(6.dp),
+            contentPadding = PaddingValues(bottom = 20.dp)
         ) {
 
-            HomeQuote(
-                quote = "WE FIRST MAKE OUR HABITS AND THEN OUR HABITS MAKE US.",
-                author = "Anonymus",
-                imageId = R.drawable.onboarding1
-            )
-
-            Row (
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ){
-
-                Text(
-                    text = "Habits". uppercase(),
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = MaterialTheme.colorScheme.tertiary
+            item {
+                HomeQuote(
+                    quote = "WE FIRST MAKE OUR HABITS AND THEN OUR HABITS MAKE US.",
+                    author = "Anonymus",
+                    imageId = R.drawable.onboarding1,
+                    modifier = Modifier.padding(bottom = 13.dp, end = 20.dp)
                 )
+            }
 
-                Spacer(modifier = Modifier.width(16.dp))
+            item {
+                Row (
+                    modifier = Modifier.fillMaxWidth().padding(bottom = 13.dp, end = 20.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ){
 
-                HomeDateSelector(
-                    selectedDate = state.selectedDate,
-                    mainDate = state.currentDate,
-                    onDateClick = {
-                        viewModel.onEvent(HomeEvent.ChangeDate(it))
-                    }
+                    Text(
+                        text = "Habits". uppercase(),
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.tertiary
+                    )
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    HomeDateSelector(
+                        selectedDate = state.selectedDate,
+                        mainDate = state.currentDate,
+                        onDateClick = {
+                            viewModel.onEvent(HomeEvent.ChangeDate(it))
+                        }
+                    )
+
+                }
+            }
+
+            items(state.habits){ habit ->
+                HomeHabit(
+                    habit = habit,
+                    selectedDate = state.selectedDate.toLocalDate(),
+                    onCheckedChange = {
+                        viewModel.onEvent(HomeEvent.CompleteHabits(habit))
+                    },
+                    onHabitClick = {}
                 )
-
             }
 
         }
--- app/src/main/java/com/carlostorres/habitsapp/home/presentation/home/HomeState.kt
+++ app/src/main/java/com/carlostorres/habitsapp/home/presentation/home/HomeState.kt
@@ -1,10 +1,29 @@
 package com.carlostorres.habitsapp.home.presentation.home
 
 import com.carlostorres.habitsapp.home.domain.models.Habit
+import java.time.LocalDate
+import java.time.LocalTime
 import java.time.ZonedDateTime
 
 data class HomeState(
     val currentDate: ZonedDateTime= ZonedDateTime.now(),
     val selectedDate : ZonedDateTime = ZonedDateTime.now(),
-    val habits : List<Habit> = emptyList()
+    val habits : List<Habit> = mockHabits //emptyList()
 )
+
+private val mockHabits = (1 .. 30).map {
+
+    val dates = mutableListOf<LocalDate>()
+    if (it % 2 == 0){
+        dates.add(LocalDate.now())
+    }
+
+    Habit(
+        id = it.toString(),
+        name = "Habito $it",
+        frequency = listOf(),
+        completedDates = dates,
+        remainder = LocalTime.now(),
+        startDate = ZonedDateTime.now()
+    )
+}
